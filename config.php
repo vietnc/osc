42ced0a5b1ded0c096cd1b72af1cc62b927b6518
Nguyen Cuong Viet <vietnc@lifetimetech.vn>
2015-04-19 15:39:48 +0700

    small updates

--- config.php
+++ config.php
@@ -24,4 +24,11 @@ define('REL_WEB_URL', '/osc/');
 
 define('WEB_PATH', 'http://localhost:8080/osc/');
 
+#cache
+define('OSC_CACHE', 'memcache');
+$_cache_config[] = array(
+   'default_host'      => '127.0.0.1',
+   'default_port'      => 11211,
+   'default_weight'    => 1
+);
 ?>
